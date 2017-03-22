e767c18a16143563ea01e3a67d7e963dc5b52271
Daniel Ramos <danielramos.dcr@gmail.com>
2017-03-21 21:23:35 -0400

    Making ajax input content to the page

--- Scripts/app.js
+++ Scripts/app.js
@@ -39,17 +39,31 @@
 
     // IF to test if the user is on ABOUT ME page.
     if (aboutMe != null) {
-        // Adding the image
-        let personalImage = document.getElementById("personalImage");
-        personalImage.innerHTML = '<img src="Assets/Images/profile-pic.jpg" alt="Personal Image">';
+        let data = {};
+        // instatiate an XHR object
+        let XHR = new XMLHttpRequest();
+        // open the json file
+        XHR.open("GET", "../content.json", true);
+        // send out a call to the XHR object
+        XHR.send(null);
+        // listen for ready state to be 4
+        XHR.onreadystatechange = function() {
+            if((this.readyState === 4) && (this.status === 200)) {
+                data = JSON.parse(this.responseText);
+            }
+        };
 
-        // Ading a title before the short paragraph
-        let whoAmI = document.getElementById("whoAmI");
-        whoAmI.innerText = "Who am I?";
-
-        // Adding the text from my short paragraph that outlines mine personal mission.
-        let shortParagraph = document.getElementById("shortParagraph");
-        shortParagraph.innerText = "My name is Daniel. I am a three year experienced developer, creating web systems using Java language with object orientation. Knowledge of Javascript, HTML5 and CSS3 technologies, together with Oracle DataBase. Now, I am expanding my knowledge on web development and taking a Web Desgin course at Georgian College hoping in the future to be a better developer, coder, thinker and person.";
+        // wait until the data is finished loading before injecting the data
+        XHR.addEventListener("load", function () {
+            // Setting the variables for the elements on the page
+            let personalImage = document.getElementById("personalImage");
+            let whoAmI = document.getElementById("whoAmI");
+            let shortParagraph = document.getElementById("shortParagraph");
+            // getting the data from the Json File
+            personalImage.innerHTML = data.index[0].image;
+            whoAmI.innerText = data.index[0].title;
+            shortParagraph.innerText = data.index[0].text;
+        });
     }
 
     // CONTACT PAGE
@@ -76,21 +90,41 @@
 
     // IF to test if the user is on PROJECTSs page.
     if (projects != null) {
-        // Project 1
-        let project1captionH4 = document.getElementById("project1captionH4");
-        let project1captionP = document.getElementById("project1captionP");
-        project1captionH4.innerText = 'Project: Movie Poster';
-        project1captionP.innerText = 'Replicate a movie poster using online HTML and CSS.';
-        // Project 2
-        let project2captionH4 = document.getElementById("project2captionH4");
-        let project2captionP = document.getElementById("project2captionP");
-        project2captionH4.innerText = 'Project: Navigation with lists';
-        project2captionP.innerText = 'Sample website using lists.';
-        // Project 3
-        let project3captionH4 = document.getElementById("project3captionH4");
-        let project3captionP = document.getElementById("project3captionP");
-        project3captionH4.innerText = 'Project: Responsive design';
-        project3captionP.innerText = 'Reponsive layout good for any type of device.'
+        let data = {};
+        // instatiate an XHR object
+        let XHR = new XMLHttpRequest();
+        // open the json file
+        XHR.open("GET", "../content.json", true);
+        // send out a call to the XHR object
+        XHR.send(null);
+        // listen for ready state to be 4
+        XHR.onreadystatechange = function() {
+            if((this.readyState === 4) && (this.status === 200)) {
+                data = JSON.parse(this.responseText);
+            }
+        };
+
+        // wait until the data is finished loading before injecting the data
+        XHR.addEventListener("load", function () {
+            let projectListBody = document.getElementById("projects");
+            //
+            data.projects.forEach(function(project) {
+                // ejecting the content for the projects
+                let newRow = document.createElement("div");
+                newRow.innerHTML = 
+                `<div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        ${project.image}
+                        <div class="caption">
+                            <h4>${project.title}</h4>
+                            <p>${project.description}</p>
+                            <p><a href="${project.url}" class="btn btn-primary" target="_blank" role="button">Check the work</a></p>
+                        </div>
+                    </div>
+                </div>`;
+                projectListBody.appendChild(newRow);
+            }, this);
+        });
     }
      
 })();                          
